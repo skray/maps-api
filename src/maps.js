2b95b8447b916eb7010e16e768745592f0f2b4f9
Sean Ray <seankennethray@gmail.com>
2016-01-05 23:05:22 -0600

    Added get and post to local postgres

--- src/maps.js
+++ src/maps.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('mydb', 'postgres', 'password', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+var Map = sequelize.define('map', {
+    name: {
+        type: Sequelize.STRING
+    }
+});
 
 router.get('/', function(req, res) {
-  res.send('some maps');
+    Map.findAll().then(function(maps) {
+        res.json(maps);
+    });
 });
+
+router.post('/', function(req, res) {
+    Map.create(req.body).then(function(map) {
+        res.status(201).json(map);
+    });
+});
+
+
 
 module.exports = router;
